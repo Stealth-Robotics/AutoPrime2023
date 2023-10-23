efc8abdbdaa59ee26f5c273a316cd321d4c59baa
Stealth Robotics <stealthrobotics@gmail.com>
2023-10-23 16:39:48 -0700

    Finished up the CameraSubsystem and TeamPropDetection subsystems but probably still need to implement them somewhere

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/CameraSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/CameraSubsystem.java
@@ -3,16 +3,40 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.subsystems.pipelines.TeamPropDetection;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
 public class CameraSubsystem extends SubsystemBase {
     private final OpenCvCamera webcam;
+    private final TeamPropDetection pipeline;
+
+    private static final int CAMERA_WIDTH = 320;
+    private static final int CAMERA_HEIGHT = 240;
 
     public CameraSubsystem(HardwareMap hardwareMap) {
-        //intialize the camera
+        //initialize the camera
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
 
+        pipeline = new TeamPropDetection();
+        webcam.setPipeline(pipeline);
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(CAMERA_WIDTH, CAMERA_HEIGHT, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                // This will be called if the camera could not be opened
+            }
+        });
+    }
+
+    public int GetConePosition() {
+        return pipeline.GetConePosition();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/pipelines/TeamPropDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/pipelines/TeamPropDetection.java
@@ -1,35 +1,61 @@
 package org.firstinspires.ftc.teamcode.subsystems.pipelines;
 
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 public class TeamPropDetection extends OpenCvPipeline {
     //The bounds of each cam region
-    private static final Point TopLeft0 = new Point(40,20);
-    private static final Point BottomRight0 = new Point(20 + TopLeft0.x,20 + TopLeft0.y);
-
-    private static final Point TopLeft1 = new Point(80,20);
-    private static final Point BottomRight1 = new Point(20 + TopLeft1.x,20 + TopLeft1.y);
-
-    private static final Point TopLeft2 = new Point(120,20);
-    private static final Point BottomRight2 = new Point(20 + TopLeft2.x,20 + TopLeft2.y);
+    private static final Point Size = new Point(20,20);
 
     private static final Rect[] cameras = {
-            new Rect(TopLeft0,BottomRight0),
-            new Rect(TopLeft1,BottomRight1),
-            new Rect(TopLeft2,BottomRight2)
+            new Rect(new Point(40,20), Size),
+            new Rect(new Point(80,20), Size),
+            new Rect(new Point(120,20), Size)
     };
 
     private volatile int conePosition = 0;
 
     @Override
     public Mat processFrame(Mat input) {
-        //TODO
-        //Figure out what the most blue/red camera is to figure out what to do during auto
-
-
+        //figures out which has the most red/blue
+        Scalar[] camRegions = new Scalar[3];
+        for (int i = 0; i < 3; i++){
+            camRegions[i] = Core.sumElems(input.submat(cameras[i]));
+        }
+        double maxColor = Math.max(Math.max(camRegions[0].val[0],camRegions[1].val[0]),camRegions[2].val[0]);
+        //draws boxes and sets value
+        for(int i = 0; i < 3; i++){
+            Point corner = new Point(cameras[i].x, cameras[i].y);
+            Point corner2 = new Point(cameras[i].x + cameras[i].width, cameras[i].y + cameras[i].height);
+            if(maxColor == camRegions[i].val[0]){
+                //sets value
+                conePosition = i;
+                Imgproc.rectangle(
+                        input,
+                        corner,
+                        corner2,
+                        new Scalar(0,255,0),
+                        2
+                );
+            }
+            else{
+                Imgproc.rectangle(
+                        input,
+                        corner,
+                        corner2,
+                        new Scalar(0,0,0),
+                        2
+                );
+            }
+        }
         return input;
+    }
+    public int GetConePosition(){
+        return conePosition;
     }
 }
