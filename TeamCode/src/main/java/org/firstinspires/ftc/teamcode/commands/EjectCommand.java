6b50cbe5fefa06b580f02aec039b2c5cfdfe99e5
4089Software <stealthrobotics@gmail.com>
2023-11-03 17:02:36 -0700

    Fixed EjectCommand -Olivia

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/EjectCommand.java
@@ -0,0 +1,14 @@
+
+package org.firstinspires.ftc.teamcode.commands;
+
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+
+import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
+
+public class EjectCommand extends SequentialCommandGroup{
+    public EjectCommand(IntakeSubsystem intake){
+        addCommands(new InstantCommand(()-> intake.SetIntakePower(1)), new WaitCommand(1000), new InstantCommand(()-> intake.SetIntakePower(0)));
+    }
+}
